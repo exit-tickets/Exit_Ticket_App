dc750e8ccd6717ac8afcf69fca92cbe5c4f90bf3
Dana Czinsky <DanaCzinsky@gmail.com>
2015-12-17 13:47:37 -0500

    creates instructor show, fixes instructor model

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -2,5 +2,5 @@ class Cohort < ActiveRecord::Base
   has_many :students
   has_many :exit_tickets
   has_one :producer
-  has_and_belongs_to_many :instructors #creates join table
+  has_one :instructor
 end
