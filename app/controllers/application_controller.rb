b750455b100d362d42d4ead241faf872939bd8b1
Dana Czinsky <DanaCzinsky@gmail.com>
2015-12-16 21:31:30 -0500

    creates user helper methods, logout button, header in layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,23 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def current_instructor
+    @current_instructor ||= Instructor.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_instructor
+
+  def current_student
+    @current_student ||= Student.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_student
+
+  def current_producer
+    @current_producer ||= Producer.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_producer
+
 end
