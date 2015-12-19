3cc17b49a83980559e178f46b790b1877bb3552f
Dana Czinsky <DanaCzinsky@gmail.com>
2015-12-19 18:44:47 -0500

    fixes creates exit ticket forms, fixes responses forms, pseudocodes rest of path

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -5,15 +5,22 @@ class ResponsesController < ApplicationController
 
 	def new 
 		@questions = Question.where(exit_ticket_id: params[:exit_ticket_id])
-		@exit_ticket = Question.find(:exit_ticket_id)
+		@exit_ticket = ExitTicket.find_by(id: params[:exit_ticket_id])
+		@student = Student.find(session[:user_id]) if session[:user_id]
 	end
 
 	def create 
-		@response = Response.create({
-			question_id: params[:question_id],
-			student_id: params[:student_id],
-			response: params[:response]
+		@responses = Response.where(exit_ticket_id: params[:exit_ticket_id])
+		@responses.each do |response|
+			response = Response.create({
+				question_id: params[:question_id],
+				student_id: params[:student_id],
+				exit_ticket_id: params[:exit_ticket_id],
+				response: params[:response]
 			})
+		end
+		@exit_ticket = ExitTicket.find(:exit_ticket_id)
+		redirect_to :new
 	end
 
 
