9f30ba20e8bbc532c47bc040ec96a220fade0aab
Dana Czinsky <DanaCzinsky@gmail.com>
2015-12-20 12:45:26 -0500

    fixes responses

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -4,23 +4,32 @@ class ResponsesController < ApplicationController
 	end
 
 	def new 
-		@questions = Question.where(exit_ticket_id: params[:exit_ticket_id])
 		@exit_ticket = ExitTicket.find_by(id: params[:exit_ticket_id])
-		@student = Student.find(session[:user_id]) if session[:user_id]
+		@questions = Question.where(exit_ticket_id: params[:exit_ticket_id])
+		@response = Response.new
+		# @responses = Response.where(exit_ticket_id: params[:exit_ticket_id])
+		# @student = Student.find(session[:user_id]) if current_student
 	end
 
 	def create 
-		@responses = Response.where(exit_ticket_id: params[:exit_ticket_id])
-		@responses.each do |response|
-			response = Response.create({
-				question_id: params[:question_id],
-				student_id: params[:student_id],
-				exit_ticket_id: params[:exit_ticket_id],
-				response: params[:response]
+		# binding.pry
+		# @responses = Response.where(exit_ticket_id: params[:exit_ticket_id])
+		# @responses.each do |response|
+		# 	response = Response.create({
+		# 		question_id: params[:question_id],
+				# student_id: params[:student_id],
+		# 		exit_ticket_id: params[:exit_ticket_id],
+		# 		response: params[:response]
+		# 	})
+		# end
+
+		@response = Response.create({
+			response: params[:response], 
+			exit_ticket_id: params[:exit_ticket_id], 
+			question_id: params[:question_id]
 			})
-		end
-		@exit_ticket = ExitTicket.find(:exit_ticket_id)
-		redirect_to :new
+		@exit_ticket = ExitTicket.find(params[:exit_ticket_id])
+		redirect_to new_exit_ticket_response_path(@exit_ticket)
 	end
 
 
