e3a129b7f04b089465677d47d696e8498143d3fb
Dana Czinsky <DanaCzinsky@gmail.com>
2015-12-21 16:24:02 -0500

    fixes bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,12 +80,12 @@ Rails.application.configure do
 
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
- :enable_starttls_auto => true,
- :address              => "smtp.gmail.com",
- :port                 => 587,
- :authentication       => :plain,
- :domain               => ENV['GMAIL_SMTP_USER'],
- :user_name            => ENV['GMAIL_SMTP_USER'],
- :password             => ENV['GMAIL_SMTP_PASSWORD']
- }
+  :enable_starttls_auto => true,
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :authentication       => :plain,
+  :domain               => ENV['GMAIL_SMTP_USER'],
+  :user_name            => ENV['GMAIL_SMTP_USER'],
+  :password             => ENV['GMAIL_SMTP_PASSWORD']
+  }
 end
