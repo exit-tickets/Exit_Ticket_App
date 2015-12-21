0fd749748d8ee30c1eee08b73599a6ab16f57304
Emily Oppenheimer <opemily@gmail.com>
2015-12-21 11:48:33 -0500

    set assets to false

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,8 @@ module ExitTicketApp
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    config.active_record.raise_in_transactional_callbacks = tru
   end
+
+  config.assets.initialize_on_precompile = false
 end
